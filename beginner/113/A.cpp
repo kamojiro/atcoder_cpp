9e5c36045c26a10f49f5150d9e920247a36efc9d
kamojiro <tamagoma002@yahoo.co.jp>
2018-11-05 21:29:36 +0900

    最近やったやつをC++で書き直し中

---
+++ beginner/113/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int X,Y;
+  cin >> X >> Y;
+  cout << X + Y/2 << "\n";
+  return 0;
+}
